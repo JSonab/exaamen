03811cc0d87c5d8ed8be44e7ec7014a46246f4cd
JSonab <136849443+JSonab@users.noreply.github.com>
2023-12-22 17:06:45 +0300

    Add files via upload

---
+++ Program12345678.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Var6 {
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пароль для проверки: ");
+            string password = Console.ReadLine();
+
+
+            if (password.Length < 8)
+            {
+                Console.WriteLine("Не подходит. Минимальная длина - 8 символов");
+            }
+            else if (!Regex.IsMatch(password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&*_.-+])[A-Za-z\d@#$%^&*_-]+$"))
+            {
+                Console.WriteLine("В пароле должны быть строчные и прописные буквы, цифры, специальные символы");
+            }
+            else
+            {
+                Console.WriteLine("Подходит");
+            }
+
+            Console.ReadKey();
+
+      
+        }
+    }
+}
+
+    
